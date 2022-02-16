0a9551d1202e870a42827328c83e900cbf4039c7
TrueArclight <50048257+TrueArclight@users.noreply.github.com>
2022-02-16 15:01:45 +0300

    Graphics LR1

---
+++ Graphics/LR1.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+from pylab import mpl
+
+x = [100, 121,  144]
+y = [10,11, 12]
+ 
+def ParametersOfLagrangeInterpolation(data_x,data_y,size):
+    parameters=[]
+         #i используется для управления количеством параметров
+    i=0
+    while i < size:
+                 #jПеременная, используемая для управления циклом, умножается
+        j = 0
+        temp = 1
+        while j < size:
+            if(i != j):
+                temp*=data_x[i]-data_x[j]
+            j+=1
+        parameters.append(data_y[i]/temp)
+        i += 1
+    return parameters
+ 
+def CalculateTheValueOfLarangeInterpolation(data_x,parameters,x):
+    returnValue=0
+    i = 0
+    while i < len(parameters):
+        temp = 1
+        j = 0
+        while j< len(parameters):
+            if(i!=j):
+                temp *=x-data_x[j]
+            j+=1
+        returnValue += temp * parameters[i]
+        i += 1
+    return returnValue
+ 
+
+def  Draw(data_x,data_y,new_data_x,new_data_y):
+        plt.plot (new_data_x, new_data_y, label = "соответствующая кривая", color = "black")
+        plt.scatter (data_x, data_y, label = "дискретные данные", color = "red")
+        plt.scatter (115, 10.723805294764, label = "real data", color = "green")
+        mpl.rcParams['axes.unicode_minus'] = False
+        plt.title ("Данные подгонки лагранжевой интерполяции")
+        plt.legend(loc="upper left")
+        plt.show()
+ 
+parameters=ParametersOfLagrangeInterpolation(x,y,3)
+datax=[10,20,30,40,50,60,70,80,90,100,110,120,130,140,150]
+datay=[]
+for temp in datax:
+    datay.append(CalculateTheValueOfLarangeInterpolation(x,parameters,temp))
+x.append(115)
+y.append(CalculateTheValueOfLarangeInterpolation(x,parameters,115))
+Draw(x,y,datax,datay)
