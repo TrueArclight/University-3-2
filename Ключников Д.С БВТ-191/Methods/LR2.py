6262ec618d6c42912cd854e98f0d224aec4caf02
deadsxnpai <truearclight@gmail.com>
2022-03-21 12:54:00 +0300

    fix

--- Ключников Д.С БВТ-191/Methods/LR2.py
+++ Ключников Д.С БВТ-191/Methods/LR2.py
@@ -1,58 +1,93 @@
+import pylab
+import math
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
 
-pressure = 753
-alfa = 0.0013
-A = []
-R = []
+kt = 5000
+t = 5000
+ct = 4187
+Tr = 90
+r = 2.26 * 10 ** 6
+F = 10
 
-def calc_A(E_vl, T_suh, T_vl):
-    return E_vl - alfa*(T_suh -T_vl)*pressure
+gizma = 0.12
+S = 0.75
+P_null = 7900
+P_first = 7600
 
-def calc_R(A, E_suh):
-    return A/E_suh * 100
+delta_C_vh = 2
+delta_m_vh = 3.2
+delta_T_vh = 8.5
 
-def Average(l): 
-    avg = sum(l) / len(l) 
-    return avg
+C0, C1 = 10, 14
+m0, m1 = 4.5, 7
+T0, T1 = 130, 155
 
-def avg():
-    fo = []
-    po = []
-    for i in range(3):
-        a = input("Temp = ")
-        fo.append(float(a))
-    for i in range(3):
-        a = input("Temp2 = ")
-        po.append(float(a))
-    print(Average(fo))
-    print(Average(po))
-def main():
-    T_suh = []
-    T_vl =[]
-    E_vl = []
-    E_suh = []
-    for i in range(3):
-        a = input("T_suh = ")
-        T_suh.append(float(a))
-    for i in range(3):
-        a = input("T_vl = ")
-        T_vl.append(float(a))
-    for i in range(3):
-        a = input("E_vl = ")
-        E_vl.append(float(a))
-    for i in range(3):
-        a = input("E_suh = ")
-        E_suh.append(float(a))
-    for i in range(3):
-        A.append(calc_A(E_vl[i], T_suh[i], T_vl[i]))
-        R.append(calc_R(A[i], E_suh[i]))
-    for i in range(3):
-        print('A = ' + str(A[i]))
-    for i in range(3):
-        print('R = '+ str(R[i]))
-    print('T_suh avg = ' + str(Average(T_suh)))
-    print('T_vl avg = ' + str(Average(T_vl)))
+t = 0
+t_max = 500
+delta_t = 0.1
 
-#main()
-avg()
+C_vhod = 10
+m_vhod = 5
+T_vhod = 150
+
+C_vihod = []
+m_vihod = []
+T_vihod = []
+
+C, m, T, vr = [], [], [], []
+c = lambda m_vh, C_vh, C_vihod, T0, m_vt: (m_vh * C_vh - C_vihod * gizma * math.sqrt(P_null + M(m_vh, m_vt) / S - P_first) - C_vihod * dM(m_vh, m_vt, T0)) / M(m_vh, m_vt)  # из файла
+
+f = lambda C_vh, m_vh, T_vh: (m_vh * C_vh * (ct * Tr - r)) / (kt * F * (T_vh - Tr) + m_vh * (ct * Tr - r))  # из 1 лабы
+
+M = lambda m_vh, m_vt: S * ((((m_vh - m_vt) / gizma) ** 2) + P_first - P_null)
+
+dM = lambda m_vh, m_vt, T0: ((r * m_vh - r * gizma * math.sqrt(P_null + M(m_vh, m_vt) / S - P_first) - kt * F * (T0 - Tr) - ((m_vh - gizma * math.sqrt(P_null + M(m_vh, m_vt) / S - P_first)) * ct * Tr)) / (r - ct * Tr))  # из файла
+
+cc = f(C0, m_vhod, T_vhod)
+print(cc)
+C0 += delta_C_vh
+m_vt = m_vhod - ((m_vhod * C0) / cc)
+
+while t < t_max:
+    t = t + delta_t
+    vr.append(t)
+
+t = 0
+while t < t_max:
+    t = t + delta_t
+    cc = cc + (c(m_vhod, C0, cc, T_vhod, m_vt) * delta_t)
+    C.append(cc)
+print(C)
+
+t = 0
+cc = f(C_vhod, m0, T_vhod)
+m0 += delta_m_vh
+m_vt = m0 - ((m0 * C_vhod) / cc)
+while t < t_max:
+    t = t + delta_t
+    cc = cc + c(m0, C_vhod, cc, T_vhod, m_vt) * delta_t
+    m.append(cc)
+
+t = 0
+cc = f(C_vhod, m_vhod, T0)
+T0 += delta_T_vh
+m_vt = m_vhod - ((m_vhod * C_vhod) / cc)
+while t < t_max:
+    t = t + delta_t
+    cc = cc + c(m_vhod, C_vhod, cc, T0, m_vt) * delta_t
+    T.append(cc)
+
+fig, axes = pylab.subplots(nrows=1, ncols=3, figsize=(12, 5))
+
+axes[0].set(title='С.вых (C.вх)')
+axes[1].set(title='С.вых (m.вх)')
+axes[2].set(title='С.вых (T.вх)')
+axes[0].plot(vr, C, color='blue')
+axes[1].plot(vr, m, color='blue')
+axes[2].plot(vr, T, color='blue')
+
+pylab.subplots_adjust(wspace=0.5, hspace=0)
+pylab.show()
