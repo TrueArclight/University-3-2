c14a12cb65d39e5cc40e246f0b5e37649f2bf965
deadsxnpai <truearclight@gmail.com>
2022-03-28 15:06:47 +0300

    [LR3] +Methods and some fixes

--- Ключников Д.С БВТ-191/Methods/LR3.py
+++ Ключников Д.С БВТ-191/Methods/LR3.py
@@ -34,26 +34,36 @@ l = [i for i in np.arange(0, L + delta_l, delta_l)]
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
 
 while T_vhod_0 < T_vhod_1:
+
     C1 = C_vh
     C2 = 0
+    
     T1 = T_vhod_0   
-    C1_percent = C1 * (100*0.2805) / ro 
-    C2_percent = C2 * (100*0.2805) / ro 
+
+    C1_percent = C1 * (100 * 0.2805) / ro 
+    C2_percent = C2 * (100 * 0.2805) / ro 
+
     for i in l:
-        C1_percent = C1 * (100*0.2805) / ro 
-        C2_percent = C2 * (100*0.2805) / ro 
-        res_1.append(C1_percent)
+        C1_percent = C1 * (100 * 0.2805) / ro 
+        C2_percent = C2 * (100 * 0.2805) / ro 
+        res_1.append(C1_percent)    
         res_2.append(C2_percent)
-        C1 = C1 + d_C1(C1,T1) * delta_l
-        C2 = C2 + d_C2(C1,C2,T1,T1) * delta_l
+        C1 = C1 + d_C1(C1, T1) * delta_l
+        C2 = C2 + d_C2(C1, C2, T1, T1) * delta_l
+
     axes[0].plot(l, res_1, color='black')
-    axes[1].plot(l, res_2, color='black')
     axes[0].set_xlabel("L, метры")
     axes[0].set_ylabel("C1, %")
+    axes[0].grid(True)
+
+    axes[1].plot(l, res_2, color='red')
     axes[1].set_xlabel("L, метры")
     axes[1].set_ylabel("C2, %")
+    axes[1].grid(True)
+
     res_1.clear()
     res_2.clear()
+
     T_vhod_0 += delta_T
 
 
