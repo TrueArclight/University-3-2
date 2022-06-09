d3ca5528b6c276a446bc30cca8155312cf01e099
deadsxnpai <truearclight@gmail.com>
2022-06-09 12:35:44 +0000

    message commit

---
+++ Klyuchnikov_D_S_BVT-191/Information systems/LR3/client.py
@@ -0,0 +1,51 @@
+import socket
+import random
+import time
+import zlib
+from Protocol import check_hash
+
+
+def can_bit_array():
+    can_array = [1, 11, 1, 1, 0, 4]
+    iteration = 0
+    while iteration < 116:
+        can_array.append(random.randint(0, 128))
+        iteration += 1
+    can_array.append(15)
+    can_array.append(1)
+    can_array.append(1)
+    can_array.append(7)
+    can_array.append(7)
+    return bytearray(can_array)
+
+
+def can_sender(client):
+    secs = 0.1
+    allPackets = 0
+    Packets = 0
+    while allPackets <= 270:
+        allPackets += 1
+        Packets += 1
+        can_array = can_bit_array()
+        client_hash = ""
+        for sym in str(zlib.crc32(can_array)):
+            if len(client_hash) != 2:
+                client_hash += sym
+        can_array.append(int(client_hash))
+        # can_array = check_hash(can_array, secs)
+        client.send(can_array)
+        time.sleep(secs)
+        print("Задержка ", int(secs * 1000), "мс\n")
+        print("Пакетов отправлено: ", allPackets)
+        if Packets == 3:
+            secs -= 0.001
+            Packets = 0
+
+
+def connection():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(("192.168.0.100", 51017))
+    print("Подключение к серверу произошло успешно")
+    can_sender(client)
+
+connection()
---
+++ Klyuchnikov_D_S_BVT-191/Information systems/LR3/server.py
@@ -0,0 +1,71 @@
+import socket
+import zlib
+import matplotlib.pyplot as plt
+
+
+def can_receiver(client_data):
+    secs = 0.1
+    speed = 0
+    allPackets = 0
+    lostPackets = 0
+    Packets = 0
+    list_lost_packets = list()
+    list_speed = list()
+    while allPackets <= 270:
+        allPackets += 1
+        Packets += 1
+        can_array = bytearray(client_data.recv(1024))
+        try:
+            client_hash = can_array.pop()
+            server_hash = ""
+            for sym in str(zlib.crc32(can_array)):
+                if len(server_hash) != 2:
+                    server_hash += sym
+            if client_hash == int(server_hash):
+                print("Пакетов принято: ", allPackets, "| Пакетов потеряно: ", lostPackets)
+                print("Задержка ", int(secs * 1000), "мс\n")
+            else:
+                print("!!!!!!!!!!!!!Пакет поврежден!!!!!!!!!!!!!!\n")
+                lostPackets += 1
+                print("Пакетов принято: ", allPackets, "| Пакетов потеряно: ", lostPackets)
+                print("Задержка ", int(secs * 1000), "мс\n")
+            if Packets == 3:
+                secs -= 0.001
+                speed += 0.3
+                Packets = 0
+            list_lost_packets.append(lostPackets)
+            list_speed.append(speed)
+        except:
+            print("||||||||||||||| Пакет вообще провалился хрен пойми куда |||||||||||||||\n")
+            lostPackets += 1
+            print("Пакетов принято: ", allPackets, "| Пакетов потеряно: ", lostPackets)
+            print("Задержка ", int(secs * 1000), "мс\n")
+            list_lost_packets.append(lostPackets)
+            list_speed.append(speed)
+    print(list_lost_packets)
+    print(list_speed)
+    return list_speed, list_lost_packets
+
+
+def plotting(cords_x, cords_y):
+    fig, ax = plt.subplots()
+    ax.plot(cords_x, cords_y,)
+    ax.set_xlabel("мс")
+    ax.set_ylabel("Потерянные пакеты")
+    plt.show()
+
+def connection():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('0.0.0.0', 51017))
+    server.listen(True)
+    print("Сервер ожидает подключение клиента")
+    client_data, client_address = server.accept()
+    print("Клиент подключен ", client_address)
+    list_cords_x, list_cords_y = can_receiver(client_data)
+    plotting(list_cords_x, list_cords_y)
+
+connection()
+
+
+
+
